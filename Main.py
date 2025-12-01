3f1064e984e3a8925e6cc97c87673dffd524a846
Swastik Raj <swastik.ojas@gmail.com>
2025-12-01 12:07:32 -0600

    test 2

--- Main.py
+++ Main.py
@@ -7,19 +7,24 @@ from datetime import datetime
 from autogen_ext.models.ollama import OllamaChatCompletionClient
 client = OllamaChatCompletionClient(model="llama3.2",)
 
-def user_calendar(date: int, event: str ):
+events = []
+
+def user_calendar():
     """
     The function Ask the user for the month, day, and year of an event to be saved, which will be stored in a list
     that will can be 
     """
-    events = []
-    event_month = int(input('Select Month: '))
-    event_day = int(input('Select Day: '))
+    event_month = int(input("Select Month : "))
+    event_day = int(input("Select Day : "))
     event_year = 2025
-    event_desc = input('Describe the Event to be saved: ')
+    event_desc = input("Describe the event to be saved: ")
     date = datetime(event_year, event_month, event_day)
-    events = [event_desc, date]
-    return events
+    event = {
+        "description": event_desc,
+        "date": date
+    }
+    events.append(event)
+    return event
 
 
 tool_agent = ToolAgent(
